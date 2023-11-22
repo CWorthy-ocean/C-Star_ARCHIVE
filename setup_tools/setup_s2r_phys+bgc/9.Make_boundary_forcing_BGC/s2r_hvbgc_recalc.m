3229ae31ff1dbd1968b7c9e2e340531c29471299
Scott Bachman <bachman@ucar.edu>
2023-11-21 18:28:00 -0700

    Tools/Externals for running C-Star
    Modified s2r phys+bgc scripts so that they do not use calc_dic1 when
    making boundary forcing. They now just interpolate directly from GLODAP
    for Alk and DIC.

--- setup_tools/setup_s2r_phys+bgc/9.Make_boundary_forcing_BGC/s2r_hvbgc_recalc.m
+++ setup_tools/setup_s2r_phys+bgc/9.Make_boundary_forcing_BGC/s2r_hvbgc_recalc.m
@@ -1,6 +1,6 @@
 function s2r_hvbgc_recalc(BGC_INI,grdname,bryname,chdscd,obcflag);
 %--------------------------------------------------------------
-%  Produce a ROMS boundary file from SODA 2.0.4 data 
+%  Produce a ROMS boundary file from SODA 2.0.4 data
 %
 %  Inspired by Roms_tools (IRD).
 %  Thanks to Pierrick, Patrick (IRD), Francois (UCLA), Yusuke (UCLA)
@@ -14,18 +14,18 @@ function s2r_hvbgc_recalc(BGC_INI,grdname,bryname,chdscd,obcflag);
   N_c       = chdscd.N;
   scoord_c  = chdscd.scoord;
 
-  
+
   [npc mpc] = size(ncread(grdname,'h'));
-  
+
 
  for bnd = 1:4
-     
+
   disp('-------------------------------------------------------------')
   if ~obcflag(bnd)
     disp('Closed boundary')
     continue
   end
-  if bnd==1 
+  if bnd==1
    disp('South boundary')
    i0 = 1;
    i1 = npc;
@@ -33,7 +33,7 @@ function s2r_hvbgc_recalc(BGC_INI,grdname,bryname,chdscd,obcflag);
    j1 = 2;
    suffix='_south';
   end
-  if bnd==2 
+  if bnd==2
    disp('East boundary')
    i0 = npc-1;
    i1 = npc;
@@ -41,7 +41,7 @@ function s2r_hvbgc_recalc(BGC_INI,grdname,bryname,chdscd,obcflag);
    j1 = mpc;
    suffix='_east';
   end
-  if bnd==3 
+  if bnd==3
    disp('North boundary')
    i0 = 1;
    i1 = npc;
@@ -49,7 +49,7 @@ function s2r_hvbgc_recalc(BGC_INI,grdname,bryname,chdscd,obcflag);
    j1 = mpc;
    suffix='_north';
   end
-  if bnd==4 
+  if bnd==4
    disp('West boundary')
    i0 = 1;
    i1 = 2;
@@ -58,24 +58,24 @@ function s2r_hvbgc_recalc(BGC_INI,grdname,bryname,chdscd,obcflag);
    suffix='_west';
   end
 
-  
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
-  
-[C,trc]=find(ismember(BGC_INI.bgc_tracer,'DIC')==1) ;  
-   if (C==1)         
-       disp('Computing DIC and Alk from surface pCO2')
-       [srf_NTA_vol,srf_dic_vol]=det_sfc_dic_nta_BRY(bryname,grdname,chdscd,bnd) ;
-       dic_alk_profile_BRY(bryname,grdname,chdscd,srf_NTA_vol,srf_dic_vol,-750,bnd) ;   
-   end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-   vec = strfind(BGC_INI.bgc_frctype,'s') ; 
+%[C,trc]=find(ismember(BGC_INI.bgc_tracer,'DIC')==1) ;
+%   if (C==1)
+%       disp('Computing DIC and Alk from surface pCO2')
+%       [srf_NTA_vol,srf_dic_vol]=det_sfc_dic_nta_BRY(bryname,grdname,chdscd,bnd) ;
+%       dic_alk_profile_BRY(bryname,grdname,chdscd,srf_NTA_vol,srf_dic_vol,-750,bnd) ;
+%   end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+   vec = strfind(BGC_INI.bgc_frctype,'s') ;
    for trc=1:length(vec)
        if vec{trc}==1
-       scaled_tracer = BGC_INI.bgc_tracer{trc} ; 
-       ref_ind       = str2num(BGC_INI.bgc_frctype{trc}(3:end)) ; 
-       tracer_scaled = BGC_INI.bgc_tracer{ref_ind} ; 
+       scaled_tracer = BGC_INI.bgc_tracer{trc} ;
+       ref_ind       = str2num(BGC_INI.bgc_frctype{trc}(3:end)) ;
+       tracer_scaled = BGC_INI.bgc_tracer{ref_ind} ;
        factor        = str2num(BGC_INI.bgc_frcini{trc}) ;
        disp(['SCALING variable ' scaled_tracer ' from ' tracer_scaled ' and factor ' num2str(factor)])
        var = ncread(bryname,[tracer_scaled suffix]) ;
