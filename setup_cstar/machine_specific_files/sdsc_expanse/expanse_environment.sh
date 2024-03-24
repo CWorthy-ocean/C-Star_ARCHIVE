05491b9576cc5c91301cbd2528d99f84233ffd6e
“Dafydd <dafydd_stephenson@hotmail.com>
2024-03-24 13:18:54 -0600

    add help message to setup script

---
+++ setup_cstar/machine_specific_files/sdsc_expanse/expanse_environment.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+################################################################################
+# MODULES
+module purge
+# Restore default SDSC modules
+module load slurm sdsc DefaultModules shared
+
+# - Prerequisite modules for netcdf (as told by using 'module spider netcdf')
+module load cpu/0.15.4  intel/19.1.1.217  mvapich2/2.3.4
+# - netCDF-c
+module load netcdf-c/4.7.4
+# - netCDF-Fortran
+module load netcdf-fortran/4.5.3
+# - ncview
+module load ncview/2.1.8
+################################################################################
+# ENVIRONMENT
+export ROMS_ROOT=$(pwd)/externals/ucla-roms/
+export MARBL_ROOT=$(pwd)/externals/MARBL/
+
+export PATH=$PATH:$ROMS_ROOT/Tools-Roms
+
+# - set roms' environment variables to match expanse module paths:
+# 1 - ucla-roms compilation
+export NETCDFHOME=$NETCDF_FORTRANHOME
+export MPIHOME=$MVAPICH2HOME
+# 2 - older roms verions compilation
+export NETCDF=$NETCDF_FORTRANHOME
+export MPI_ROOT=$MVAPICH2HOME
+
+################################################################################
